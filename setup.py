ffdc0ddc48d96262dfb4e99751026f168e0e08b9
Rahil <rahil@oracle.com>
2025-12-15 12:21:54 +0530

    vehicle project setup with conda env

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Naveen Kumar",
+    author_email="naveensddp1@gmail.com",
+    packages=find_packages()
+)
